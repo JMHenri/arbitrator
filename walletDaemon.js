60c40f60e6d948ddc002c8af1f572d6c4242b06b
Jacob <JMHenri@users.noreply.github.com>
2025-02-17 09:33:46 -0500

    Update walletDaemon.js

--- walletDaemon.js
+++ walletDaemon.js
@@ -5,9 +5,6 @@ var Liqui = require('node.liqui.io');
 //publicClient.getProducts(callback);
 //all
 
-var apiSecret = '83dd85b534b2371b8db5445510a8dd7115d11eb6e73a7db117812740833457c7';
-var apiKey="4VNMI4N1-ZNNGVF7J-FYTZZ9RP-4TRCTLQ8-FP4L75I6";
-
 
 let liqui = new Liqui(apiKey, apiSecret);
 
