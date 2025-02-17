57174720d737952e5e7a9ae7620d3c0782c28710
Jacob <JMHenri@users.noreply.github.com>
2025-02-17 09:33:36 -0500

    Update testMarket.js

--- testMarket.js
+++ testMarket.js
@@ -7,11 +7,6 @@ var events = require('events');
 
 var Liqui = require('node.liqui.io');
 
-
-
-var apiSecret = '83dd85b534b2371b8db5445510a8dd7115d11eb6e73a7db117812740833457c7';
-var apiKey="4VNMI4N1-ZNNGVF7J-FYTZZ9RP-4TRCTLQ8-FP4L75I6";
-
 let liqui = new Liqui(apiKey, apiSecret);
 
 
